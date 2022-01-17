065ad77f0d79360d8996969afe849aa9756e96f7
Bojo <bozhidar_zahariev@hotmail.com>
2022-01-17 21:29:01 +0000

    search

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const description = document.getElementById('description');
 
 getPics = async () => {
   try {
-    const response = await fetch("https://api.unsplash.com/photos/random/?client_id=uR_SC9lsIk-CaquQQ0F92ZEqvvxmgbvn3mC8T64stGQ", {
+    const response = await fetch("https://api.unsplash.com/search/photos/?client_id=uR_SC9lsIk-CaquQQ0F92ZEqvvxmgbvn3mC8T64stGQ&query=dog", {
+
+      // const response = await fetch("https://api.unsplash.com/photos/random?client_id=uR_SC9lsIk-CaquQQ0F92ZEqvvxmgbvn3mC8T64stGQ", {
      
-     
+     //api.unsplash.com/search/photos?query=wanderlust 
+    //  https://api.unsplash.com/search/photos?page=1&query=SEARCH_QUERY
     });
     const fetchedData = await response.json();
-    console.log(fetchedData);
-    imgHolder.src = fetchedData.urls.regular;
+    console.log(fetchedData.results);
+    imgHolder.src = fetchedData.results[0].urls.regular;
     
-    if(fetchedData.description) {
+    if(fetchedData[0].description) {
       
-    description.textContent = fetchedData.description;
+    description.textContent = fetchedData[0].description;
     }
 
 
