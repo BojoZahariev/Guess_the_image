506cef4583212b3a111eb96ae92d04c69f001602
Bojo <bozhidar_zahariev@hotmail.com>
2022-01-21 21:04:35 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const imgHolder = document.getElementById('imgHolder');
-
-const description = document.getElementById('list');
+const coverDiv= document.getElementById('coverDiv');
+const list = document.getElementById('list');
 
 getPics = async (word) => {
   try {
     const response = await fetch("https://api.unsplash.com/search/photos/?client_id=uR_SC9lsIk-CaquQQ0F92ZEqvvxmgbvn3mC8T64stGQ&orientation=landscape&query=" + word, {
 
-      // const response = await fetch("https://api.unsplash.com/photos/random?client_id=uR_SC9lsIk-CaquQQ0F92ZEqvvxmgbvn3mC8T64stGQ", {
      
-     //api.unsplash.com/search/photos?query=wanderlust 
-    //  https://api.unsplash.com/search/photos?page=1&query=SEARCH_QUERY
     });
     const fetchedData = await response.json();
     console.log(fetchedData.results);
@@ -22,18 +19,42 @@ getPics = async (word) => {
       getWords();
     }
     
-    
-    // if(fetchedData[0].description) {
-      
-    // description.textContent = fetchedData[0].description;
-    // }
-
-
   } catch (err) {
     console.log(err);
 
   }
 };
+
+
+
+getPic2 =  (word) => {
+  fetch("https://api.pexels.com/v1/search?query=" + word + "&per_page=1", {
+	
+	"headers": {
+		"authorization": "563492ad6f917000010000017a53b03de08849d79c05b0cf2e44a408"
+	}
+})
+.then(resp => {
+  return resp.json()
+})
+.then(data => {
+  console.log(data);
+  if(data.photos.length > 0) {
+    imgHolder.style.backgroundImage = "url(" + data.photos[0].src.landscape + ")";
+    list.style.display = 'block';
+  }else{
+    getWords();
+  }
+  
+})
+.catch(err => {
+	console.error(err);
+});
+};
+
+
+
+
 
 getWords = async () => {
   try {
@@ -49,7 +70,7 @@ getWords = async () => {
     
     makeList(fetchedData);
 
-    getPics(fetchedData[0]);
+    getPic2(fetchedData[0]);
   } catch (err) {
     console.log(err);
 
@@ -63,8 +84,15 @@ data.forEach(element => {
   let listElem = document.createElement('li');
   listElem.textContent = element;
   list.appendChild(listElem);
+
+listElem.addEventListener("click", function(){
+  coverDiv.style.display = 'none';
+});
+
+
 });
 }
+
 
 
 
