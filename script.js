ce52e9ef79b09a223cb068fcb996cb2ac939caf1
Bojo <bozhidar_zahariev@hotmail.com>
2022-01-21 19:27:57 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const imgHolder = document.getElementById('imgHolder');
-const description = document.getElementById('description');
 
-getPics = async () => {
+const description = document.getElementById('list');
+
+getPics = async (word) => {
   try {
-    const response = await fetch("https://api.unsplash.com/search/photos/?client_id=uR_SC9lsIk-CaquQQ0F92ZEqvvxmgbvn3mC8T64stGQ&query=dog", {
+    const response = await fetch("https://api.unsplash.com/search/photos/?client_id=uR_SC9lsIk-CaquQQ0F92ZEqvvxmgbvn3mC8T64stGQ&orientation=landscape&query=" + word, {
 
       // const response = await fetch("https://api.unsplash.com/photos/random?client_id=uR_SC9lsIk-CaquQQ0F92ZEqvvxmgbvn3mC8T64stGQ", {
      
@@ -12,12 +13,20 @@ getPics = async () => {
     });
     const fetchedData = await response.json();
     console.log(fetchedData.results);
-    imgHolder.src = fetchedData.results[0].urls.regular;
-    
-    if(fetchedData[0].description) {
+
+    if(fetchedData.results.length) {
+      imgHolder.src = fetchedData.results[0].urls.regular;
+
       
-    description.textContent = fetchedData[0].description;
+    }else{
+      getWords();
     }
+    
+    
+    // if(fetchedData[0].description) {
+      
+    // description.textContent = fetchedData[0].description;
+    // }
 
 
   } catch (err) {
@@ -26,7 +35,38 @@ getPics = async () => {
   }
 };
 
+getWords = async () => {
+  try {
+    const response = await fetch("https://random-word-form.herokuapp.com/random/noun?count=5", {
+
+      // const response = await fetch("https://api.unsplash.com/photos/random?client_id=uR_SC9lsIk-CaquQQ0F92ZEqvvxmgbvn3mC8T64stGQ", {
+     
+     //api.unsplash.com/search/photos?query=wanderlust 
+    //  https://api.unsplash.com/search/photos?page=1&query=SEARCH_QUERY
+    });
+    const fetchedData = await response.json();
+    console.log(fetchedData);
+    
+    makeList(fetchedData);
+
+    getPics(fetchedData[0]);
+  } catch (err) {
+    console.log(err);
+
+  }
+};
+
+
+function makeList(data) {
+  list.innerHTML = '';
+data.forEach(element => {
+  let listElem = document.createElement('li');
+  listElem.textContent = element;
+  list.appendChild(listElem);
+});
+}
 
 
 
-getPics();
+getWords();
+// getPics();
